8308f42e402a3135634399f879fea7710cc75519
Dora Gil <git@doragil.me>
2019-07-06 14:59:37 -0400

    Added GoBack function w/Nate

--- src/Components/NotesSidebar.js
+++ src/Components/NotesSidebar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NotesSidebar = (props) => {
     return ( 
         <nav>
-            <button>Go Back</button>
+            <button onClick={props.goBack}>Go Back</button>
             <h3>{props.folder}</h3>
         </nav>
      );
