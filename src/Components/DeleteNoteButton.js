38958ed982b77f2fb44ab384ed56086e5363d6bb
Dora Gil <git@doragil.me>
2019-07-13 13:35:24 -0400

    Added Delete Note Button Styling w/Nate

--- src/Components/DeleteNoteButton.js
+++ src/Components/DeleteNoteButton.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { NotefulContext } from './NotefulContext'
+import { NotefulContext } from './NotefulContext';
+import './DeleteNoteButton.css'
 
 const DeleteNoteButton = (props) => {
     return ( 
         <NotefulContext.Consumer>
             {({deleteNote}) => (
-            <button onClick={() => deleteNote(props.noteId)}>Delete Note</button>
+            <button className="btn btnDelete" onClick={() => deleteNote(props.noteId)}>Delete Note</button>
             )}
         </NotefulContext.Consumer>
      );
