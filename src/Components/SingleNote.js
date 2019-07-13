c759d94a10abfc3592767a9d8cba7a5e29c3117b
Dora Gil <git@doragil.me>
2019-07-13 14:12:39 -0400

    Added singlenote component styling w/Nate

--- src/Components/SingleNote.js
+++ src/Components/SingleNote.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import NoteItem from './NoteItem'
 import PropTypes from 'prop-types';
+import './SingleNote.css'
 
 const SingleNote = (props) => {
     return ( 
-        <section>
+        <section className="indNote">
           <ul>
             <NoteItem id={props.note.id} name={props.note.name} modified={props.note.modified}  />
           </ul>
