c9191603fbb09cd422819fde2c8b774f57e3d402
Dora Gil <git@doragil.me>
2019-07-13 13:36:07 -0400

    Added Note Item Styling  w/Nate

--- src/Components/NoteItem.js
+++ src/Components/NoteItem.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import DeleteNoteButton from './DeleteNoteButton';
 import PropTypes from 'prop-types';
+import './NoteItem.css'
 
 const NoteItem = (props) => {
-
+    
     return ( 
-                <li><Link to={`/note/${props.id}`}><h2>{props.name}</h2></Link>
-                <span>Date Modified on {props.modified}</span>
+                <li className="noteItem"><Link to={`/note/${props.id}`}><h2>{props.name}</h2></Link>
+                <span className="noteDate">Date Modified on {new Date(props.modified).toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric'  })}</span>
                 <DeleteNoteButton noteId={props.id} />
             </li>
      );
